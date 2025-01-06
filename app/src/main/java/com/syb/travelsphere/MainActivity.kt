447f4093c88adc08a419b64cbd110fd2bd55015f
boazAvami <boaz.avami@gmail.com>
2025-01-06 21:35:29 +0200

    working

--- app/src/main/java/com/syb/travelsphere/MainActivity.kt
+++ app/src/main/java/com/syb/travelsphere/MainActivity.kt
@@ -1,42 +1,55 @@
-// MainActivity.kt
 package com.syb.travelsphere
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.syb.travelsphere.pages.AllPostsFragment
-import com.syb.travelsphere.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.syb.travelsphere.pages.AllPostsFragment
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Load the AllPostsFragment by default
-        if (savedInstanceState == null) {
-            loadFragment(AllPostsFragment())
-        }
+        // Set up the Toolbar (ActionBar)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar) // This sets the toolbar as the ActionBar
 
-        // Set up BottomNavigationView with NavController
-        // val navController = findNavController(R.id.nav_host_fragment)
-        // val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        // bottomNavigationView.setupWithNavController(navController)
+        // Set up NavController with BottomNavigationView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        val navController = findNavController(R.id.nav_host_fragment) // Use the correct fragment ID
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.nearbyTravellersFragment,
+                R.id.newPostFragment,
+                R.id.profileFragment
+            )
+        )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
 
+        // Set up BottomNavigationView with NavController
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
+
+        // If savedInstanceState is null, set the default fragment using navigation
+        if (savedInstanceState == null) {
+            navController.navigate(R.id.homeFragment)
+        }
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
+    // Override onSupportNavigateUp to handle up navigation
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
