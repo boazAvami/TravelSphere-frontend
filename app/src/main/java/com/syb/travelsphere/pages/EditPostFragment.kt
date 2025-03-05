28db7454e2d20bfedc6a11bdd4ebf458db26f078
ShirinYa <shirinyashar@gmail.com>
2025-03-05 21:32:13 +0200

    start fixing

--- app/src/main/java/com/syb/travelsphere/pages/EditPostFragment.kt
+++ app/src/main/java/com/syb/travelsphere/pages/EditPostFragment.kt
@@ -1,23 +1,15 @@
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.firebase.firestore.FirebaseFirestore
-import com.syb.travelsphere.R
 import com.syb.travelsphere.databinding.FragmentSinglePostBinding
-import com.syb.travelsphere.model.FirebaseModel
 import com.syb.travelsphere.model.Model
 import com.syb.travelsphere.model.Post
 
 class EditPostFragment : Fragment() {
     private var binding: FragmentSinglePostBinding? = null
     private lateinit var post: Post
-    private var fireBase = FirebaseModel();
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -28,47 +20,31 @@ class EditPostFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        super.onViewCreated(view, savedInstanceState)
 
-        // Load post data
-        //todo: get it from somewhere
-        // maybe:         Model.shared.getPostById(postId)
-        var userName = fireBase.getUserById(post.ownerId) { user ->
-            binding?.userNameText?.text = user?.userName
-            // Load the profile picture from base64
-            val base64Image = user?.profilePictureUrl
-            if (!base64Image.isNullOrEmpty()) {
-                try {
-                    val bitmap = decodeBase64Image(base64Image)
-                    binding?.userProfilePicture?.setImageBitmap(bitmap)
-                } catch (e: Exception) {
-                    // If decoding fails, set a default image
-                    binding?.userProfilePicture?.setImageResource(R.drawable.default_user)
+        //todo: get data from the constructor / from model
+        post;
+        Model.shared.getUserById(post.ownerId) { user ->
+            binding?.userNameText?.text = user.value?.userName
+            user.value?.profilePictureUrl?.let { it1 ->
+                Model.shared.getImageByUrl(it1) { image ->
+                    run {
+                        binding?.userProfilePicture?.setImageBitmap(image)
+                    }
                 }
-            } else {
-                // Set default image if no profile picture is provided
-                binding?.userProfilePicture?.setImageResource(R.drawable.default_user)
             }
-        }
-        // Display the post data
-        // Load the profile picture from base64
-        //todo: make it work with Url
-        val base64Image = post.imageUrl
-        if (!base64Image.isNullOrEmpty()) {
-            try {
-                val bitmap = decodeBase64Image(base64Image)
-                binding?.photoViewPager?.setImageBitmap(bitmap)
-            } catch (e: Exception) {
-                // If decoding fails, set a default image
-                binding?.photoViewPager?.setImageResource(R.drawable.default_user)
-            }
-        } else {
-            // Set default image if no profile picture is provided
-            binding?.photoViewPager?.setImageResource(R.drawable.default_user)
         }
 
-//        binding?.locationNameText?.text = post.locationName
+        binding?.locationNameText?.text = post.locationName
         binding?.descriptionText?.text = post.description
         binding?.timestampText?.text = "Created on: ${post.creationTime}"
+        post.photos[0].let { it1 ->
+            Model.shared.getImageByUrl(it1) { image ->
+                run {
+                    binding?.photoViewPager?.setImageBitmap(image)
+                }
+            }
+        }
 
         // Show edit button in edit mode
         binding?.editButton?.visibility = View.VISIBLE
@@ -76,31 +52,15 @@ class EditPostFragment : Fragment() {
         // Handle Edit Button Click
         binding?.editButton?.setOnClickListener {
             val newDescription = binding?.descriptionText?.text.toString()
-            var newPost = post;
+            val newLocationName = binding?.locationNameText?.text.toString().trim()
+
             //todo: put the new data
-            //newPost.description = newDescription;
-            updatePostDescription(newPost)
+            var newPost = post.copy(
+                description = newDescription,
+                locationName = newLocationName
+            )
+            //todo: is that correct??
+            Model.shared.editPost(newPost) {}
         }
-    }
-
-    private fun updatePostDescription(newPost: Post) {
-        //take the wright model to update
-        //todo: is that correct??
-        val postRef = FirebaseFirestore.getInstance().collection("posts").document(newPost.id)
-
-        Model.shared.editPost(post) {}
-        postRef.update("description", newPost.description)
-            .addOnSuccessListener {
-                Toast.makeText(requireContext(), "Post updated!", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener {
-                Toast.makeText(requireContext(), "Update failed!", Toast.LENGTH_SHORT).show()
-            }
-    }
-
-    // Helper function to decode base64 string to Bitmap
-    private fun decodeBase64Image(base64String: String): Bitmap {
-        val decodedString = Base64.decode(base64String, Base64.DEFAULT)
-        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
     }
 }
--- app/src/main/java/com/syb/travelsphere/pages/SettingsFragment.kt
+++ app/src/main/java/com/syb/travelsphere/pages/SettingsFragment.kt
@@ -1,11 +1,8 @@
 package com.syb.travelsphere.pages
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Base64
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,17 +10,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
-import com.syb.travelsphere.R
 import com.syb.travelsphere.auth.AuthManager
 import com.syb.travelsphere.databinding.FragmentSettingsBinding
-import com.syb.travelsphere.model.FirebaseModel
 import com.syb.travelsphere.model.Model
 import com.syb.travelsphere.model.User
 
 class SettingsFragment : Fragment() {
     private var binding: FragmentSettingsBinding? = null
     private lateinit var authManager: AuthManager  // Declare AuthManager
-    private val firebaseModel = FirebaseModel()
     private val imagePickerRequestCode = 1001
 
     override fun onCreateView(
@@ -58,22 +52,17 @@ class SettingsFragment : Fragment() {
         val currentUser = authManager.getCurrentUser()
         if (currentUser != null) {
 
-            val userInfo = firebaseModel.getUserById(currentUser.uid, { user ->
-                if (user != null) {
+            Model.shared.getUserById(currentUser.uid, { liveData ->
+                if (liveData.value != null) {
                     binding?.emailText?.setText(currentUser.email)
-                    binding?.phoneText?.setText(user.phoneNumber)
-                    binding?.usernameText?.setText(user.userName)
-                    binding?.locationSwitch?.isChecked = user.isLocationShared == true
-
-                    // Load the profile picture from base64
-                    val base64Image = user.profilePictureUrl
-                    if (!base64Image.isNullOrEmpty()) {
-                        try {
-                            val bitmap = decodeBase64Image(base64Image)
-                            binding?.profileImage?.setImageBitmap(bitmap)
-                        } catch (e: Exception) {
-                            // If decoding fails, set a default image
-                            binding?.profileImage?.setImageResource(R.drawable.default_user)
+                    binding?.phoneText?.setText(liveData.value?.phoneNumber)
+                    binding?.usernameText?.setText(liveData.value?.userName)
+                    binding?.locationSwitch?.isChecked = liveData.value?.isLocationShared == true
+                    liveData.value?.profilePictureUrl?.let { it1 ->
+                        Model.shared.getImageByUrl(it1) { image ->
+                            run {
+                                binding?.profileImage?.setImageBitmap(image)
+                            }
                         }
                     }
                 }
@@ -101,17 +90,16 @@ class SettingsFragment : Fragment() {
 
             val currentUser = authManager.getCurrentUser()
             if (currentUser != null) {
-                firebaseModel.getUserById(currentUser.uid) { user ->
-                    if (user != null) {
+                Model.shared.getUserById(currentUser.uid) { user ->
+                    if (user.value != null) {
                         // Create updated user object (email is not modified)
-                        val updatedUser = user.copy(
+                        val updatedUser = user.value?.copy(
                             phoneNumber = updatedPhone,
                             userName = updatedUsername,
                             isLocationShared = isLocationShared  // ✅ Add location sharing status
                         )
 
-
-                        updatedUser.profilePictureUrl?.let { it1 ->
+                        updatedUser?.profilePictureUrl?.let { it1 ->
                             Model.shared.getImageByUrl(it1) { image ->
                                 Model.shared.editUser(
                                     updatedUser,
@@ -120,7 +108,6 @@ class SettingsFragment : Fragment() {
                             }
                         }
                     }
-
 
                     // Show confirmation (optional)
                     Toast.makeText(
@@ -131,7 +118,6 @@ class SettingsFragment : Fragment() {
                 }
             }
         }
-
 
         return binding?.root
     }
@@ -147,12 +133,6 @@ class SettingsFragment : Fragment() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         intent.type = "image/*"
         startActivityForResult(intent, imagePickerRequestCode)
-    }
-
-    // Helper function to decode base64 string to Bitmap
-    private fun decodeBase64Image(base64String: String): Bitmap {
-        val decodedString = Base64.decode(base64String, Base64.DEFAULT)
-        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
     }
 
     override fun onDestroyView() {
